b237db8a0542a1b4362c5eddaad1d171b139d63f
samzek <sam@Einaudi>
2015-03-31 18:54:08 +0200

    bug fix in Print_LCS

--- LCS.c
+++ LCS.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef enum Direction {UP,HOR,DIAG} Direction;
+typedef enum Direction {UP,HOR,DIAG,HORUP} Direction;
 
 typedef struct matrix_t {
 	int len;
@@ -24,42 +24,40 @@ for (j = 0; j < lenB;j++)
 
 for(i = 1; i < lenA;i++){
     for (j = 1; j < lenB;j++){
-
 		if (A[i] == B[j]){
 			m[j + (i * lenB)].len = m[(j-1) + (i-1)*lenB].len + 1;
+			//printf("DIAG A[i]=%c B[i]=%c len=%d\n",A[i],B[j],m[j + (i * lenB)].len);
 			m[j + (i*lenB)].dir = DIAG;
 		}
 		else if (m[j + (i-1) * lenB].len >= m[(j-1) + (i*lenB)].len){
 			m[j + i*lenB].len = m[j + (i-1)*lenB].len;
+		//	printf("UP A[i]=%c B[i]=%c len=%d\n",A[i],B[j],m[j + (i * lenB)].len);
 			m[j + i*lenB].dir = UP;
 		}
-		else{
+		else {
 			m[j + i*lenB].len = m[(j-1) + i*lenB].len;
+                  //   printf("HOR A[i]=%c B[i]=%c len=%d\n",A[i],B[j],m[j + (i * lenB)].len);
 			m[j + i*lenB].dir = HOR;
-			}	
+		}	
 	}
 }					
 return m;
 };
 
-char *app;
-int p=0;
-void Print_LCS(Matr *m,int i,int j,char *A){
+void Print_LCS(Matr *m,int i,int j,char *A,int lenB){
 
 if ((i == 0) || (j ==0))
 	return;
-if (m[j + (i*strlen(A))].dir == DIAG){
-	Print_LCS(m,i-1,j-1,A);
-	app[p] = A[i];
-	p++;
+if (m[j + (i*lenB)].dir == DIAG){
+	Print_LCS(m,i-1,j-1,A,lenB);
+	printf("%c",A[i]);
 }
-else if (m[j+(i*strlen(A))].dir == UP)
-	Print_LCS(m,i-1,j,A);
+else if (m[j+(i*lenB)].dir == UP)
+	Print_LCS(m,i-1,j,A,lenB);
 else
-	Print_LCS(m,i,j-1,A);
+	Print_LCS(m,i,j-1,A,lenB);
 
 };
-
 
 int main(int argc,char **argv){
 
@@ -85,15 +83,13 @@ while (fgets(line, 1024, file)) {
 		for (i = 0;i < endB;i++)
 			B[i+1] = line [endA+1 + i];
 		B[0] = '0';
+	//	printf("%s;%s\n",A,B);
 		lcs = LCS_Length(A,B);
-		app = (char *) malloc (lcs[endB + endA*strlen(A)].len*sizeof(char));
-		Print_LCS(lcs,endA,endB,A);
-		if(strlen(app) != 0){
-			printf("%s",app);
+		//printf("len A=%d;len B=%d\n",strlen(A),strlen(B));
+		//printf("lcs[lenA][lenB]=%d\n",lcs[strlen(B)-1+(strlen(A)-1)*(strlen(B)-1)].len);
+		Print_LCS(lcs,strlen(A)-1,strlen(B)-1,A,strlen(B));
+		if(lcs[endB + endA*strlen(B)].len > 0)
 			printf("\n");
-		}
-		p = 0;
-		free(app);				    		
 	}	
 }
 return 0;
